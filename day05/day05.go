fbf9d3bf62ec2ed70280ad4be4832ffb8434749b
Squidysquid1 <46660298+Squidysquid1@users.noreply.github.com>
2024-12-04 12:20:17 -0600

    add fillers for days

---
+++ day05/day05.go
@@ -0,0 +1,48 @@
+package day05
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// Solution for star 1 first attempt
+func SolveStar1Naive() int {
+	// Read the input file
+	file, err := os.Open("day05/input.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return -1
+	}
+	defer file.Close()
+
+	// Process the input file line by line
+	var scanner = bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+
+	}
+
+	return 0
+}
+
+// Solution for star 2 first attempt
+func SolveStar2Naive() int {
+	// Read the input file
+	file, err := os.Open("day05/simpleinput.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return -1
+	}
+	defer file.Close()
+
+	// Process the input file line by line
+	var scanner = bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
+
+	return 0
+}
